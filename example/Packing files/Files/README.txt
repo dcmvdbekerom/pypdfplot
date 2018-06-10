19267f55119104d5351bdf206c4dfffa9b7971ea
s060340 <dcmvdbekerom@gmail.com>
2018-06-10 16:02:29 +0200

    File comparison & TODOs
    Gives a warning now when you are about to lose data
    
    TODOs are added here and there
    
    added linebreaks before the version header

---
+++ example/Packing files/Files/README.txt
@@ -0,0 +1,1 @@
+These are spare copies of the files in case you inadvertedly deleted them.
--- pypdfplot/__init__.py
+++ pypdfplot/__init__.py
@@ -5,12 +5,13 @@ import os
 import sys
 import datetime
 import pickle
+import binascii
 
 LINE_LENGTH = 128
 
 cleanup_list = []
 packed_files = []
-existing_files  = []
+collision_list = []
 file_package = ''
 file_table   = ''
 file_ptr = 0
@@ -20,10 +21,10 @@ base,ext = os.path.splitext(pyname)
 
 if pyname != '':
     with open(pyname,'rb') as f:
-        fbuf = f.read().replace('\n','\n')
+        fbuf = f.read()
         __fbuf = fbuf
 
-        ## find revision
+        ## Find revision
         eos = fbuf.find('')
         rev_str = fbuf[eos+1:].split('\n')[3]
 
@@ -32,23 +33,26 @@ if pyname != '':
         else:
             rev = 1
 
-        ## unpack files
+        ## Unpack files
         try:
             ipdf = fbuf[eos+1:].find('%PDF') + eos + 1
             i1   = fbuf[ipdf:].find('\n')+ipdf+2
-            table_size = int(fbuf[i1   :i1+10])
-            n_files    = int(fbuf[i1+11:i1+21])
-
-            table = fbuf[i1+21:i1+table_size+21].split('\n')[1:-1]
-
+            i2   = i1 + 32
+            table_size  = int(fbuf[i1   :i1+10])
+            n_files     = int(fbuf[i1+11:i1+21])
+            line_length = int(fbuf[i1+22:i1+32])
+            
+            table = fbuf[i2:i2+table_size].split('\n')[1:]
+            
             for line in table:
                 cols   = line.split('|')
                 fname  = cols[0][1:]
-                begin  = int(cols[1]) + i1 + 20 + table_size
-                end    = int(cols[2]) + begin
-
-                buf = fbuf[begin:end]
+                begin    = int(cols[1]) + i2 + 1 + table_size
+                end      = int(cols[2]) + begin
+                crc_pack = int(cols[3])
                 
+                #TODO: this formatting is not safe for text files
+                buf = fbuf[begin:end] 
                 trailer = buf[-7:].replace('%','')
                 pibuf   = buf[1:-7].replace('\n%','')+trailer
 
@@ -57,7 +61,11 @@ if pyname != '':
                         f.write(pickle.loads(pibuf))
                     print 'File "'+fname+'" unpacked...'
                 else:
-                    existing_files.append(fname)
+                    with open(fname,'rb') as f:
+                        buf = f.read()
+                    crc_local = binascii.crc32(buf)
+                    if crc_pack != crc_local:
+                        collision_list.append(fname)
                     print 'File "'+fname+'" already exists, skip unpacking...'
 
         except(ValueError):
@@ -68,7 +76,7 @@ if pyname != '':
     print '~~~'
 else:
     print "[WARNING] Python must be run from script, not from interpreter..."
-        
+
 def pack(fnames,cleanup = False):
     global file_package,file_table,file_ptr
 
@@ -78,6 +86,10 @@ def pack(fnames,cleanup = False):
     for fname in fnames:
         with open(fname,'rb') as f:
             buf = f.read()
+
+        crc = binascii.crc32(buf)
+
+        #TODO: This formatting may lead to errors with the trailer
         temp_buf = pickle.dumps(buf,0)
         pibuf = ''
         while len(temp_buf)>LINE_LENGTH:
@@ -90,15 +102,15 @@ def pack(fnames,cleanup = False):
         file_package += pibuf
         packed_files.append(fname)
 
-        file_table += '%{:s}|{:d}|{:d}\n'.format(fname,file_ptr,len(pibuf))
+        file_table += '%{:s}|{:d}|{:d}|{:d}\n'.format(fname,file_ptr,len(pibuf),crc)
         file_ptr   += len(pibuf)
         
         if cleanup:
             cleanup_list.append(fname)
 
         print 'File "'+fname+'" marked for packaging'+(' & deletion in folder' if cleanup else '')+'...'
-                
-
+    return                
+    
 def __append_pdf(fbuf,extra_bytes):
     
     extra_len = len(extra_bytes)
@@ -141,7 +153,10 @@ def publish(cleanup = ['all','none','individual'][2],
     global file_table,file_package
 
     py_file = __fbuf[:eos]
-        
+
+    while(py_file[-2] != '\n'):
+        py_file += '\n'
+
     now = datetime.datetime.now()
     version_info = " ### -+- Don't remove this line -+- ###\n"+\
                    " ###  |    Date:    {:}   |  ###\n".format(now.strftime("%Y-%m-%d"))+\
@@ -151,7 +166,7 @@ def publish(cleanup = ['all','none','individual'][2],
                    '"""\n'
 
     ## Prepare packing: 
-    file_table = '%{:010} {:010d}\n'.format(len(file_table),len(packed_files)) + file_table
+    file_table = '%{:010} {:010d} {:010d}\n'.format(len(file_table),len(packed_files),LINE_LENGTH) + file_table
     file_package = file_table + file_package
 
     ## Save the plot:
@@ -165,7 +180,6 @@ def publish(cleanup = ['all','none','individual'][2],
     plot_file = __append_pdf(plot_file,file_package)
 
     ## Create the new combined file
-
     if sys.argv[0] == '':
         savename = input('Please specify filename:\n')
         if savename[:-4] != '.pdf':
@@ -184,8 +198,11 @@ def publish(cleanup = ['all','none','individual'][2],
         f.write(sbuf)
 
     ## Cleanup:
+    close_editor_warning = False
     if os.path.exists(savename):    
         for fname in cleanup_list:
+            if fname in collision_list:
+                close_editor_warning = True
             os.remove(fname)
 
         if inplace:
@@ -193,13 +210,18 @@ def publish(cleanup = ['all','none','individual'][2],
                 os.remove(pyname)
 
     ## Output:
-
     print '~~~\nPublished file: "'+savename+'"...!'
     print 'Packed {:d} file'.format(len(packed_files))+('s...' if len(packed_files) >1 else '...')
     print 'Revision: '+str(rev)
     print '~~~\n'
-    print 'You can now close the Python editor...'
+    if close_editor_warning:
+        #TODO: Use warnings module for warnings
+        print '[WARNING] Packed files have been updated and deleted in folder.'
+        print '[WARNING] You must CLOSE this editor WITHOUT SAVING to prevent loss of data.'
+        print '[WARNING] Saving and re-running the script will overwrite the updated files with the older ones.'
+    else:
+        print 'You can now close the Python editor...'
 
     if do_show:
         show()
-    return 0
+    return
