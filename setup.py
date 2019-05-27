5c8cf9ad391703eca26aeeef345bb17345e27647
Dirk van den Bekerom <dcmvdbekerom@gmail.com>
2019-05-27 14:33:47 -0400

    preparing for upload to testPyPI

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='pypdfplot',
-      version='0.3',
+      version='0.3.1',
       description="Saves plots as PDF with embedded generating script",
       author='Dirk van den Bekerom',
       author_email='dcmvdbekerom@gmail.com',
       license='MIT',
+      url = 'https://github.com/dcmvdbekerom/pypdfplot',
       packages=['pypdfplot'],
       install_requires=['matplotlib','PyPDF2'],
       zip_safe=False)
