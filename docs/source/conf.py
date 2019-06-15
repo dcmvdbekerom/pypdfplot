7ff313b8e9b87be4a56fe3c3716a700efd112240
Dirk van den Bekerom <dcmvdbekerom@gmail.com>
2019-06-15 18:30:23 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Dirk van den Bekerom'
 author = 'Dirk van den Bekerom'
 
 # The short X.Y version
-version = 0.3
+version = '0.3'
 # The full version, including alpha/beta/rc tags
-release = 0.3.6
+release = '0.3.6'
 
 # -- General configuration ---------------------------------------------------
 
