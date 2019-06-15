73d3de70e0e9f81886bf8ede76ad6ecadeccb919
Dirk van den Bekerom <dcmvdbekerom@gmail.com>
2019-06-15 17:09:11 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup
 
 setup(name='pypdfplot',
-      version='1.0.0',
+      version='0.3.4',
       description="Saves plots as PDF with embedded generating script",
       author='Dirk van den Bekerom',
       author_email='dcmvdbekerom@gmail.com',
       license='MIT',
-      url = 'https://github.com/dcmvdbekerom/pypdfplot',
+      #url = 'https://github.com/dcmvdbekerom/pypdfplot',
       packages=['pypdfplot'],
       install_requires=['matplotlib','PyPDF4'],
-      zip_safe=False)
+      project_urls={
+        'Documentation': 'https://pypdfplot.readthedocs.io/',
+        'GitHub': 'https://github.com/dcmvdbekerom/pypdfplot'}, 
+      zip_safe=False,
+      classifiers = [
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Science/Research",
+          "Programming Language :: Python",
+          "Topic :: Multimedia :: Graphics",
+          "Topic :: Scientific/Engineering :: Visualization"])
